e8c5197c142abac30180308ea9603ee6a1c22d95
Squeezy93 <Squeezy93@yandex.ru>
2024-03-04 18:24:13 +0300

    Add Some Code

---
+++ src/CustomCRM.Entities/SomeClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCRM.Entities
+{
+    internal class SomeClass
+    {
+        public int x = 0;
+    }
+}
